05dfa4fcf18bbc47df46a4c94988f4cc4dd99d7a
dev-Anderson1 <anderson.santos@devcons.com.br>
2025-08-03 18:57:59 -0300

    ajustes composer

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/', function () {
+    return response('OK', 200);
+});
